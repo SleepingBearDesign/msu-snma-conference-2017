05bf7d42a5623de208f108a04ce88ee3f509ce8f
Brody Berson <brody.berson@gmail.com>
2017-09-28 17:45:47 -0400

    Update timer
    Not sure why it’s off?

--- js/countdown-custom.js
+++ js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         jQuery(function () {
-            jQuery('#defaultCountdown').countdown({until: new Date(2017, 10, 27, 8)}); // year, month, date, hour
+            jQuery('#defaultCountdown').countdown({until: new Date(2017, 9, 28, 8)}); // year, month, date, hour
         });
 });		
 
